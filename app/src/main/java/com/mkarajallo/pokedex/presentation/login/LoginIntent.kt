f95a8d85df57150a993304429a5b93eafad63949
mkarajallo <mkarajallo@sodep.com.py>
2026-09-21 12:05:43 -0300

    style: format the login intent file

--- app/src/main/java/com/mkarajallo/pokedex/presentation/login/LoginIntent.kt
+++ app/src/main/java/com/mkarajallo/pokedex/presentation/login/LoginIntent.kt
@@ -1,7 +1,7 @@
 package com.mkarajallo.pokedex.presentation.login
 
 sealed class LoginIntent {
-    data class CambioUsuario(val valor: String): LoginIntent()
-    data class CambioContrasena(val valor: String): LoginIntent()
-    data object Enviar: LoginIntent()
+    data class CambioUsuario(val valor: String) : LoginIntent()
+    data class CambioContrasena(val valor: String) : LoginIntent()
+    data object Enviar : LoginIntent()
 }
